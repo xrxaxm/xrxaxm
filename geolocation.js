7644e0561cf4c4cda18dc6fb8fd5626a772605b4
xrxaxm <69647243+xrxaxm@users.noreply.github.com>
2022-08-05 15:18:08 +0700

    Create geolocation.js

---
+++ geolocation.js
@@ -0,0 +1,29 @@
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET','https://ipapi.co/json/',false);
+        xhr.send();
+        var res=JSON.parse(xhr.response);
+        document.getElementById("coun").innerHTML=res["country_name"];
+    document.getElementById("stat").innerHTML=res["region"];
+       document.getElementById("city").innerHTML=res["city"];
+        document.getElementById("sim").innerHTML=res["org"];
+        document.getElementById("region_code").innerHTML=res["region_code"];   
+        document.getElementById("ipku").innerHTML=res["ip"];      
+        document.getElementById("city").innerHTML=res["city"];
+  
+document.getElementById("country").innerHTML=res["country"];
+  document.getElementById("country_code").innerHTML=res["country_code"];
+         document.getElementById("country_code_iso3").innerHTML=res["country_code_iso3"];
+ document.getElementById("country_capital").innerHTML=res["country_capital"];
+  
+   document.getElementById("country_tld").innerHTML=res["country_tld"];
+         document.getElementById("postal").innerHTML=res["postal"];
+  document.getElementById("latitude").innerHTML=res["latitude"];
+  document.getElementById("longitude").innerHTML=res["longitude"];
+  document.getElementById("timezone").innerHTML=res["timezone"];
+         document.getElementById("country_calling_code").innerHTML=res["country_calling_code"];
+ 
+  document.getElementById("languages").innerHTML=res["languages"];
+  
+document.getElementById("country_area").innerHTML=res["country_area"];
+ 
+document.getElementById("country_population").innerHTML=res["country_population"];
